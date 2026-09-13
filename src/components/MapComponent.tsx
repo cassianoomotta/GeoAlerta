370a3238826a5f7d8705df174f1d182d47305807
cassianoomotta <cassianoomotta@gmail.com>
2026-09-13 13:12:30 -0300

    fix: import markercluster css so occurrences remain visible on zoom out

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -5,6 +5,8 @@ import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import MarkerClusterGroup from 'react-leaflet-cluster';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 
 // Pino Vermelho
 const icon = L.icon({
